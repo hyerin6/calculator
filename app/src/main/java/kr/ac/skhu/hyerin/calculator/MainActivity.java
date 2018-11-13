badae44b4bb3e4cd10143e9fe734e6e618a87b42
hyerin6 <0419hyerin@gmail.com>
2018-11-14 00:35:52 +0900

    Button->EditText 출력

--- app/src/main/java/kr/ac/skhu/hyerin/calculator/MainActivity.java
+++ app/src/main/java/kr/ac/skhu/hyerin/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    String s = "";
+    private String s = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
