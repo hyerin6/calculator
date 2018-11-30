351b9178739ee0115d5654d39dce5710cda31cad
hyerin6 <33855307+hyerin6@users.noreply.github.com>
2018-11-30 14:05:29 +0900

    코드위치변경 및 불필요한 코드 삭제

--- app/src/main/java/kr/ac/skhu/hyerin/calculator/MainActivity.java
+++ app/src/main/java/kr/ac/skhu/hyerin/calculator/MainActivity.java
@@ -1,7 +1,5 @@
 package kr.ac.skhu.hyerin.calculator;
 
-package kr.ac.skhu.kimparkjang.calculator;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
