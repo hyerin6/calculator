18658bc2be5135736ffb49db269ffab1c85e9499
hyerin6 <33855307+hyerin6@users.noreply.github.com>
2018-11-30 13:56:44 +0900

    코드위치변경

--- app/src/main/java/kr/ac/skhu/hyerin/calculator/MainActivity.java
+++ app/src/main/java/kr/ac/skhu/hyerin/calculator/MainActivity.java
@@ -1,5 +1,7 @@
 package kr.ac.skhu.hyerin.calculator;
 
+package kr.ac.skhu.kimparkjang.calculator;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,137 +11,154 @@ import android.widget.TextView;
 
 import java.util.Stack;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editText;
+    TextView textView;
     String s = "";
-    
-
-    Button.OnClickListener t;
-
-    {
-        t = new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText e = findViewById(R.id.e);
-                TextView textView = findViewById(R.id.textview);
-                String buttonStr = ((Button) v).getText().toString();
-
-
-                switch (buttonStr) {
-                    case "AC":  //AC 누르면 (이전 결과 창 제외) 전부 삭제
-                        s = "";
-                        e.setText(s);
-                        break;
-                    case "=":
-                        Stack<String> stack = new Stack<>();
-                        String[] expression = s.split(" ");
-                        String[] expression2 = new String[expression.length];
-                        int i = 0;
-                        for (String s1 : expression)
-                            switch (s1) {
-                                case "+":
-                                case "-":
-                                    if (stack.isEmpty()) {
-                                        stack.push(s1);
-                                        break;
-                                    }
-                                    while (!stack.isEmpty()) {
-                                        expression2[i] = stack.pop();
-                                        i++;
-                                    }
-                                    stack.push(s1);
-                                    break;
-                                case "×":
-                                case "/":
-                                    if (stack.isEmpty()) {
-                                        stack.push(s1);
-                                        break;
-                                    }
-                                    String operator = stack.peek();
-                                    if (operator.compareTo("×") == 0 || operator.compareTo("/") == 0) {
-                                        while (!stack.isEmpty()) {
-                                            expression2[i] = stack.pop();
-                                            i++;
-                                        }
-                                    } else {
-                                        stack.push(s1);
-                                        break;
-                                    }
-                                default:
-                                    expression2[i] = s1;
-                                    i++;
-                            }
-                        while (!stack.isEmpty()) {
-                            expression2[i] = stack.pop();
-                            i++;
-                        }
-                        int num1, num2;
-                        int result;
-                        Stack<Integer> stack2 = new Stack<>();
-                        for (i = 0; i < expression2.length; i++) {
-                            switch (expression2[i]) {
-                                case "+":
-                                    num1 = stack2.pop();
-                                    num2 = stack2.pop();
-                                    result = num1 + num2;
-                                    stack2.push(result);
-                                    break;
-                                case "-":
-                                    num1 = stack2.pop();
-                                    num2 = stack2.pop();
-                                    result = num2 - num1;
-                                    stack2.push(result);
-                                    break;
-                                case "×":
-                                    num1 = stack2.pop();
-                                    num2 = stack2.pop();
-                                    result = num1 * num2;
-                                    stack2.push(result);
-                                    break;
-                                case "/":
-                                    num1 = stack2.pop();
-                                    num2 = stack2.pop();
-                                    result = num2 / num1;
-                                    stack2.push(result);
-                                    break;
-                                default:
-                                    stack2.push(Integer.parseInt(expression2[i]));
-                                    break;
-                            }
-                        }
-                        result = stack2.pop();
-                        textView.setText(result);
-
-                        break;
-                    default:
-                        s += buttonStr + " ";
-                        e.setText(s);
-                        break;
-                }
-            }
-        };
-    }
+    String buttonStr = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button1).setOnClickListener(t);
-        findViewById(R.id.button2).setOnClickListener(t);
-        findViewById(R.id.button3).setOnClickListener(t);
-        findViewById(R.id.button4).setOnClickListener(t);
-        findViewById(R.id.button5).setOnClickListener(t);
-        findViewById(R.id.button6).setOnClickListener(t);
-        findViewById(R.id.button7).setOnClickListener(t);
-        findViewById(R.id.button8).setOnClickListener(t);
-        findViewById(R.id.button9).setOnClickListener(t);
-        findViewById(R.id.button0).setOnClickListener(t);
-        findViewById(R.id.buttonAC).setOnClickListener(t);
-        findViewById(R.id.buttonmul).setOnClickListener(t);
-        findViewById(R.id.buttonsub).setOnClickListener(t);
-        findViewById(R.id.buttonPlus).setOnClickListener(t);
-        findViewById(R.id.buttondiv).setOnClickListener(t);
-        findViewById(R.id.buttonR).setOnClickListener(t);
+        editText = findViewById(R.id.e);
+        textView = findViewById(R.id.textview);
 
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        Button button5 = findViewById(R.id.button5);
+        Button button6 = findViewById(R.id.button6);
+        Button button7 = findViewById(R.id.button7);
+        Button button8 = findViewById(R.id.button8);
+        Button button9 = findViewById(R.id.button9);
+        Button button0 = findViewById(R.id.button0);
+        Button buttonp = findViewById(R.id.buttonPlus);
+        Button buttond = findViewById(R.id.buttondiv);
+        Button buttonm = findViewById(R.id.buttonmul);
+        Button buttonre = findViewById(R.id.buttonR);
+        Button buttonac = findViewById(R.id.buttonAC);
+        Button buttons = findViewById(R.id.buttonsub);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+        button7.setOnClickListener(this);
+        button8.setOnClickListener(this);
+        button9.setOnClickListener(this);
+        button0.setOnClickListener(this);
+        buttonp.setOnClickListener(this);
+        buttonm.setOnClickListener(this);
+        buttond.setOnClickListener(this);
+        buttons.setOnClickListener(this);
+        buttonre.setOnClickListener(this);
+        buttonac.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        buttonStr = ((Button) v).getText().toString();
+
+        switch (buttonStr) {
+            case "AC":
+                editText.setText("");
+                s = "";
+                break;
+            case "=":
+                Stack<String> stack = new Stack<>();
+                String[] expression = s.split(" ");
+                String[] expression2 = new String[expression.length];
+
+                int num1, num2;
+                int result;
+                Stack<Integer> stack2 = new Stack<>();
+
+
+
+                int i=0;
+                for (String s1 : expression) {
+                    switch(s1) {
+                        case "+":
+                        case "-":
+                            if(stack.isEmpty()) {
+                                stack.push(s1);
+                                break;
+                            }
+                            while(!stack.isEmpty()) {
+                                expression2[i] = stack.pop();
+                                i++;
+                            }
+                            stack.push(s1);
+                            break;
+                        case "×":
+                        case "/":
+                            if (stack.isEmpty()) {
+                                stack.push(s1);
+                                break;
+                            }
+                            String operator = stack.peek();
+                            if (operator == "×" || operator == "/" ) {
+                                while(!stack.isEmpty()) {
+                                    expression2[i] = stack.pop();
+                                    i++;
+                                }
+                            }
+                            else {
+                                stack.push(s1);
+                                break;
+                            }
+                        default:
+                            expression2[i] = s1;
+                            i++;
+                    }
+                }
+                while(!stack.isEmpty()) {
+                    expression2[i] = stack.pop();
+                    i++;
+                }
+
+                for(i = 0; i<expression2.length; i++) {
+                    if(expression2[i].equals("+")) {
+                        num1 = stack2.pop();
+                        num2 = stack2.pop();
+                        result = num1 + num2;
+                        stack2.push(result);
+                    }
+                    else if(expression2[i].equals("-")) {
+                        num1 = stack2.pop();
+                        num2 = stack2.pop();
+                        result = num2 - num1;
+                        stack2.push(result);
+                    }
+                    else if(expression2[i].equals("×")) {
+                        num1 = stack2.pop();
+                        num2 = stack2.pop();
+                        result = num1 * num2;
+                        stack2.push(result);
+                    }
+                    else if(expression2[i].equals("/")) {
+                        num1 = stack2.pop();
+                        num2 = stack2.pop();
+                        result = num2 / num1;
+                        stack2.push(result);
+                    }
+                    else
+                        stack2.push(Integer.parseInt(expression2[i]));
+                }
+
+                result = stack2.pop();
+                textView.setText(String.valueOf(result));
+                break;
+
+            default:
+                s += buttonStr + " ";
+                editText.setText(s);
+                break;
+        }
     }
 }
